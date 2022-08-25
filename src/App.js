3276e229d8f1308822360367b9078b73887bf0ea
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-08-25 13:53:38 +0700

    initial react , add Header , add Hero , add Mostpicked

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import "./assets/scss/style.scss";
+import landingPage from "./pages/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Route path="/" component={landingPage}></Route>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
