eafcf2703257a089e1e6c6566e80a61fe02466f1
HafizhHasanAR <hafizhhafizh9@gmail.com>
2022-08-25 15:27:46 +0700

    add testimonial and footer

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -6,7 +6,8 @@ import Hero from "../parts/Hero";
 import MostPicked from "../parts/MostPicked";
 import landingPage from "../json/landingPage.json";
 import Categories from "../parts/Categories";
-
+import Testimony from "../parts/Testimony";
+import Footer from "../parts/Footer";
 export default class LandingPage extends Component {
   constructor(props) {
     super(props);
@@ -22,6 +23,8 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimony data={landingPage.testimonial} />
+        <Footer data={landingPage.footer} />
       </>
     );
   }
